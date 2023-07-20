0cc99ed273c38966d9fc0d2a7722d8cd31086abf
RealSanjeev <45820805+realsanjeev@users.noreply.github.com>
2023-07-20 23:34:09 +0545

    Update model_trainer.py

--- src/pipeline/model_trainer.py
+++ src/pipeline/model_trainer.py
@@ -14,9 +14,36 @@ from src.exception import CustomException
 
 class ModelTrainer:
     def __init__(self, config: ModelTrainerConfig):
+        '''
+        Initialize the ModelTrainer class.
+
+        Args:
+            config (ModelTrainerConfig): An instance of ModelTrainerConfig\
+                  containing the configuration parameters for the model training.
+
+        Returns:
+            None
+        '''
         self.config = config
 
-    def train(self, tokenizer=None):
+    def train(self, tokenizer=None, exists_ok=False):
+        '''
+        Train the model using the specified tokenizer and configuration.
+
+        Args:
+            tokenizer (AutoTokenizer, optional): An tokenizer for tokenizing the input data.\
+                If not provided, a default tokenizer will be used based on the model checkpoint.
+            exists_ok (bool, optional): If True, training will be skipped\
+                if the model already exists in the specified output directory. \
+                    `Default is False`.
+
+        Returns:
+            Tuple (str, str): A tuple containing the paths to the saved model and tokenizer.
+
+        Raises:
+            CustomException: If any error occurs during the training process, \
+                it will be captured and raised as a CustomException.
+        '''
         try:
             device = "cuda" if torch.cuda.is_available() else "cpu"
             logging.info(f">>>>>>>> Model trainer initiated using '{device.upper()}' <<<<<<<<<<<")
@@ -24,7 +51,14 @@ class ModelTrainer:
 
             if tokenizer is None:
                 tokenizer = AutoTokenizer.from_pretrained(self.config.model_ckpt)
-            model_pegasus = AutoModelForSeq2SeqLM.from_pretrained(self.config.model_ckpt, from_tf=True).to(device)
+            model_save_path = os.path.join(self.config.root_dir, "pegasus-samsum-model")
+
+            if os.path.exists(model_save_path) and exists_ok:
+                logging.warning(f'Model already exist in path: {model_save_path}')
+                tokenizer_save_path = os.path.join(self.config.root_dir, "tokenizer")
+                tokenizer.save_pretrained(tokenizer_save_path)
+                return model_save_path, tokenizer_save_path
+            model_pegasus = AutoModelForSeq2SeqLM.from_pretrained(self.config.model_ckpt).to(device)
             seq2seq_data_collator = DataCollatorForSeq2Seq(tokenizer, model=model_pegasus)
 
             logging.info(f"Download the pretrained model: '{self.config.model_ckpt}' completed")
@@ -55,14 +89,15 @@ class ModelTrainer:
             trainer.train()
             current_time = time.time()
 
-            logging.info(f"Model trained Successfully in {current_time-prev_time:%.2f} sec")
+            logging.info(f"Model trained Successfully in {current_time - prev_time:.2f} sec")
 
             ## Save model
-            model_save_path = os.path.join(self.config.root_dir, "pegasus-samsum-model")
             model_pegasus.save_pretrained(model_save_path)
+
             ## Save tokenizer
             tokenizer_save_path = os.path.join(self.config.root_dir, "tokenizer")
             tokenizer.save_pretrained(tokenizer_save_path)
 
         except Exception as err:
-            raise CustomException(err, sys)
+            raise CustomException(err, sys) from err
+        return model_save_path, tokenizer_save_path
