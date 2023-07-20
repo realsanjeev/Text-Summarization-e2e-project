2ec1eb3081ed16b8e644653bdf053d640e7fe038
RealSanjeev <45820805+realsanjeev@users.noreply.github.com>
2023-07-20 23:36:48 +0545

    Update data_ingestion.py

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -1,7 +1,9 @@
 import os
+import sys
 from urllib import request
 from pathlib import Path
 import zipfile
+
 from src.configuration.config import DataIngestionConfig
 from src.logger import logging
 from src.utils import get_size
@@ -12,19 +14,22 @@ class DataIngestion:
         self.config = config
 
     def download_dataset(self):
+        '''Download dataset if not eexists loacal disk'''
         if not os.path.exists(self.config.local_data_path):
             file, header = request.urlretrieve(url=self.config.source_URL,
                                                filename=self.config.local_data_path)
             logging.info(f"{file} successfully downloaded! Info: \n{header}")
         else:
-            logging.info(f"File already exists of size: {get_size(Path(self.config.local_data_path))} KB")
+            logging.info(f"File already exists of size: \
+                         {get_size(Path(self.config.local_data_path))} KB")
 
     def extract_zip_file(self, unzip_path=None):
         """Extract zip file to specified path
 
         Args:
             unzip_path: str, optional
-                The path to extract the zip file to. If not provided, it defaults to `self.config.unzip_dir`.
+                The path to extract the zip file to. If not provided,\
+                    it defaults to `self.config.unzip_dir`.
         """
         if unzip_path is None:
             unzip_path = self.config.unzip_dir
@@ -33,6 +38,7 @@ class DataIngestion:
             with zipfile.ZipFile(self.config.local_data_path, 'r') as zip_fp:
                 zip_fp.extractall(unzip_path)
                 logging.info("Dataset successfully extracted.")
-        except CustomException as err:
-            logging.warn(f"Error while extracting file path: {unzip_path}")
+        except Exception as err:
+            logging.warning(f"Error while extracting file path: {unzip_path}")
             logging.error(f"Error explaination: {err}")
+            raise CustomException(err, sys) from err
