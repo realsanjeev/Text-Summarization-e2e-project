8b3b44947817dc49a8ce161d8b7cb1010bdd7b7e
RealSanjeev <45820805+realsanjeev@users.noreply.github.com>
2023-07-20 07:53:14 +0000

    fixing warning

--- src/components/data_validation.py
+++ src/components/data_validation.py
@@ -1,4 +1,5 @@
 import os
+import sys
 from src.configuration.config import DataValidationConfig
 from src.logger import logging
 from src.exception import CustomException
@@ -6,7 +7,7 @@ from src.exception import CustomException
 class DataValidation:
     def __init__(self, config: DataValidationConfig) -> None:
         self.config = config
-    
+
     def validate_files(self):
         try:
             validation_status = False
@@ -16,11 +17,12 @@ class DataValidation:
                     validation_status = False
                 else:
                     validation_status = True
-                with open(self.config.STATUS_FILE, 'a', encoding='utf-8') as fp:
-                    fp.write(f"Validation status of {file}: {validation_status}")
-                logging.info(f"{file} Files validation Success. VALIDATION_STATUS: {validation_status}")
+                with open(self.config.STATUS_FILE, 'a', encoding='utf-8') as file_fp:
+                    file_fp.write(f"Validation status of {file}: {validation_status}")
+                logging.info(f"{file} Files validation Success. \
+                    VALIDATION_STATUS: {validation_status}")
             return validation_status
-        
+
         except Exception as err:
-            logging.warn(f"Cannot validate due to error: \n{err}")
-            raise CustomException(err, sys)
+            logging.warning(f"Cannot validate due to error: \n{err}")
+            raise CustomException(err, sys) from err
--- src/pipeline/model_trainer.py
+++ src/pipeline/model_trainer.py
@@ -1,5 +1,6 @@
 import time
 import os
+import sys
 import torch
 
 from transformers import TrainingArguments, Trainer
@@ -16,49 +17,52 @@ class ModelTrainer:
         self.config = config
 
     def train(self, tokenizer=None):
-        device = "cuda" if torch.cuda.is_available() else "cpu"
-        logging.info(f">>>>>>>> Model trainer initiated using '{device.upper()}' <<<<<<<<<<<")
-        prev_time = time.time()
+        try:
+            device = "cuda" if torch.cuda.is_available() else "cpu"
+            logging.info(f">>>>>>>> Model trainer initiated using '{device.upper()}' <<<<<<<<<<<")
+            prev_time = time.time()
 
-        if tokenizer is None:
-            tokenizer = AutoTokenizer.from_pretrained(self.config.model_ckpt)
-        model_pegasus = AutoModelForSeq2SeqLM.from_pretrained(self.config.model_ckpt).to(device)
-        seq2seq_data_collator = DataCollatorForSeq2Seq(tokenizer, model=model_pegasus)
-        
-        logging.info(f"Downloading the pretrained model '{self.config.model_ckpt}'")
-        #loading data 
-        dataset_samsum_pt = load_from_disk(self.config.data_path)
+            if tokenizer is None:
+                tokenizer = AutoTokenizer.from_pretrained(self.config.model_ckpt)
+            model_pegasus = AutoModelForSeq2SeqLM.from_pretrained(self.config.model_ckpt).to(device)
+            seq2seq_data_collator = DataCollatorForSeq2Seq(tokenizer, model=model_pegasus)
 
-        trainer_args = TrainingArguments(
-            output_dir=self.config.root_dir,
-            num_train_epochs=self.config.num_train_epochs,
-            warmup_steps=self.config.warmup_steps,
-            per_device_train_batch_size=self.config.per_device_train_batch_size,
-            per_device_eval_batch_size=self.config.per_device_train_batch_size,
-            weight_decay=self.config.weight_decay,
-            logging_steps=self.config.logging_steps,
-            evaluation_strategy=self.config.evaluation_strategy,
-            eval_steps=self.config.eval_steps,
-            save_steps=1e6,
-            gradient_accumulation_steps=self.config.gradient_accumulation_steps
-        )
+            logging.info(f"Downloading the pretrained model '{self.config.model_ckpt}'")
+            # loading data
+            dataset_samsum_pt = load_from_disk(self.config.data_path)
 
-        trainer = Trainer(model=model_pegasus,
-                          args=trainer_args,
-                          tokenizer=tokenizer,
-                          data_collator=seq2seq_data_collator,
-                          train_dataset=dataset_samsum_pt["train"],
-                          eval_dataset=dataset_samsum_pt["validation"])
-        logging.info("Model training started......")
-        trainer.train()
-        current_time = time.time()
+            trainer_args = TrainingArguments(
+                output_dir=self.config.root_dir,
+                num_train_epochs=self.config.num_train_epochs,
+                warmup_steps=self.config.warmup_steps,
+                per_device_train_batch_size=self.config.per_device_train_batch_size,
+                per_device_eval_batch_size=self.config.per_device_train_batch_size,
+                weight_decay=self.config.weight_decay,
+                logging_steps=self.config.logging_steps,
+                evaluation_strategy=self.config.evaluation_strategy,
+                eval_steps=self.config.eval_steps,
+                save_steps=1e6,
+                gradient_accumulation_steps=self.config.gradient_accumulation_steps
+            )
 
-        logging.info(f"Model trained Successfully in {current_time-prev_time:%.2f} sec")
+            trainer = Trainer(model=model_pegasus,
+                            args=trainer_args,
+                            tokenizer=tokenizer,
+                            data_collator=seq2seq_data_collator,
+                            train_dataset=dataset_samsum_pt["train"],
+                            eval_dataset=dataset_samsum_pt["validation"])
+            logging.info("Model training started......")
+            trainer.train()
+            current_time = time.time()
 
-        ## Save model
-        model_save_path = os.path.join(self.config.root_dir, "pegasus-samsum-model")
-        model_pegasus.save_pretrained(model_save_path)
-        ## Save tokenizer
-        tokenizer_save_path = os.path.join(self.config.root_dir, "tokenizer")
-        tokenizer.save_pretrained(tokenizer_save_path)
+            logging.info(f"Model trained Successfully in {current_time-prev_time:%.2f} sec")
 
+            ## Save model
+            model_save_path = os.path.join(self.config.root_dir, "pegasus-samsum-model")
+            model_pegasus.save_pretrained(model_save_path)
+            ## Save tokenizer
+            tokenizer_save_path = os.path.join(self.config.root_dir, "tokenizer")
+            tokenizer.save_pretrained(tokenizer_save_path)
+
+        except Exception as err:
+            raise CustomException(err, sys)
