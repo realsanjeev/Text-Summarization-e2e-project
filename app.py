1ff9a81e67981d9fe64c7318d4ebf0f43c27a0ad
RealSanjeev <45820805+realsanjeev@users.noreply.github.com>
2023-12-17 08:35:37 +0000

    Solving the web page render issue

--- app.py
+++ app.py
@@ -1,15 +1,23 @@
+import logging
 from flask import Flask, render_template, request
 from src.pipeline.estimator import PredictionPipeline
 
 app = Flask(__name__)
-predict_pipeline = PredictionPipeline()
+# predict_pipeline = PredictionPipeline()
+
+logging.disable(logging.CRITICAL)
 
 @app.route('/', methods=['GET', 'POST'])
 def home():
     if request.method == 'POST':
         script = request.form.get('script')
-        summary_text = predict_pipeline.predict(script)
-    return render_template('index.html', Summary=summary_text)
+        # summary_text = predict_pipeline.predict(script)
+        summary_text = script
+        return render_template('index.html',
+                               summary=summary_text,
+                               script=script)
+    
+    return render_template('index.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
--- static/main.js
+++ static/main.js
@@ -1,28 +1,29 @@
 const textarea = document.getElementById('text');
 
-const filesystem = null
+const filesystem = null;
 
 function switchToDark() {
-   document.body.style.backgroundColor = "#5d5d5d";
+  document.body.style.backgroundColor = "#5d5d5d";
   document.getElementById('text').style.backgroundColor = '#2d2d2d';
   textarea.style.backgroundColor = '#2d2d2d';
   textarea.style.color = '#7d7d7d';
-  
+
   const buttons = document.getElementsByClassName('buttons');
-  for (i=0; i<buttons.length; i++) {
+  for (i = 0; i < buttons.length; i++) {
     buttons[i].style.backgroundColor = '#3d3d3d';
- buttons[i].style.borderColor = '#8d8d8d';
+    buttons[i].style.borderColor = '#8d8d8d';
     buttons[i].style.color = '#8d8d8d';
   }
 }
+
 function switchToLight() {
   document.body.style.backgroundColor = "#ebebeb";
-textarea.style.backgroundColor = '#b5b5b5';
+  textarea.style.backgroundColor = '#b5b5b5';
   textarea.style.color = '#000000';
   const buttons = document.getElementsByClassName('buttons');
-  for (i=0; i<buttons.length; i++) {
+  for (i = 0; i < buttons.length; i++) {
     buttons[i].style.backgroundColor = '#d6d6d6';
- buttons[i].style.borderColor = '#a8a8a8';
+    buttons[i].style.borderColor = '#a8a8a8';
     buttons[i].style.color = '#616161'
   }
 }
